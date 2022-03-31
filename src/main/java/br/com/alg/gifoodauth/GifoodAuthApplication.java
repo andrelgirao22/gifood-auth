b8518613bccff41c9d3f741a3d0abcdee24628c1
andrelgirao <andrelgirao22@hotmail.com>
2022-03-31 09:26:11 -0300

    criado projeto

---
+++ src/main/java/br/com/alg/gifoodauth/GifoodAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.alg.gifoodauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifoodAuthApplication.class, args);
+	}
+
+}
