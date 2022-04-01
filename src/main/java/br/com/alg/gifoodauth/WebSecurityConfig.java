d05ca620b9d0615d8afa7685574d91bd2c26bf7e
andrelgirao <andrelgirao22@hotmail.com>
2022-04-01 09:03:31 -0300

    configurado geracao de token e check_token

--- src/main/java/br/com/alg/gifoodauth/WebSecurityConfig.java
+++ src/main/java/br/com/alg/gifoodauth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.alg.gifoodauth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -25,7 +26,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
