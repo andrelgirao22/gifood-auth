bc4b15e02c9d4566b2281c6f2709f76be650b5f8
andrelgirao <andrelgirao22@hotmail.com>
2022-04-19 09:00:52 -0300

    Criado Claims personalizados

--- src/main/java/br/com/alg/gifoodauth/domain/security/AuthUser.java
+++ src/main/java/br/com/alg/gifoodauth/domain/security/AuthUser.java
@@ -8,15 +8,18 @@ import br.com.alg.gifoodauth.domain.model.Usuario;
 import lombok.Getter;
 
 @Getter
-public class AuthUser extends User{
+public class AuthUser extends User {
 
 	private static final long serialVersionUID = 1L;
 	
 	private String fullName;
 
+	private Long userId;
+
 	public AuthUser(Usuario usuario) {
 		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
 		
+		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
 	}
 	
